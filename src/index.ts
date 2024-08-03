d183814fffc6e37a3f8a78acae6b97d7d7c15d18
Open Cipher <8209379+open-cipher@users.noreply.github.com>
2024-08-03 12:05:43 +0530

    feat: create first release

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,49 @@
+import { WeberBrowser } from '@hive-o/weber';
+import * as async from 'async';
+
+export interface Navigation {
+  query_params: URLSearchParams;
+  url: URL;
+}
+
+export class Xss {
+  async scan(urls: Navigation[], payloads: string[]) {
+    await async.forEachSeries(payloads, async (payload: string) => {
+      await async.forEachSeries(urls, async ({ url }) => {
+        const browser = WeberBrowser.instance();
+        await browser.launch();
+
+        const context = browser.context;
+        const page = await context.newPage();
+
+        try {
+          console.log(`scanning ${url} | payload ${payload}`);
+
+          page.on('dialog', async (dialog) => {
+            console.log('Found Vulnerability: ', dialog.message());
+            await dialog.dismiss();
+          });
+
+          page.on('error', console.error);
+          url.searchParams.append('query', payload);
+
+          await page.goto(url.toString(), {
+            timeout: 20000,
+            waitUntil: 'networkidle2',
+          });
+
+          await page.waitForFunction(() => document.readyState === 'complete', {
+            timeout: 20000,
+          });
+        } catch (e) {
+          console.error(`Error processing ${url.toString()}:`, e); // Log specific URL
+        } finally {
+          await page.close();
+          await browser.close();
+          console.log(`scanning ${url} | payload ${payload} completed`);
+        }
+      });
+    });
+  }
+}
 
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
   root: __dirname,
-  cacheDir: './node_modules/.vite/standalone-template',
+  cacheDir: './node_modules/.vite/xss',
 
   plugins: [nxViteTsPaths()],
 
@@ -19,6 +19,6 @@ export default defineConfig({
     environment: 'node',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     reporters: ['default'],
-    coverage: { reportsDirectory: './coverage/standalone-template', provider: 'v8' },
+    coverage: { reportsDirectory: './coverage/xss', provider: 'v8' },
   },
 });
